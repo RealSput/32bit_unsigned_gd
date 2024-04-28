dadcced0b69507ceb28d7d2d587383daf635781b
your friendly trustworthy programmer <117580169+RealSput@users.noreply.github.com>
2024-04-27 22:55:49 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,35 @@
+require('@g-js-api/g.js');
+
+let bits = 32;
+let bin1 = Array(bits).fill(0).map(x => counter(x));
+bin1.forEach((x, i) => x.display(30 * i, 105));
+
+let c2 = counter(0);
+c2.subtract(2 ** 31 - 1);
+let c3 = counter(0);
+let c4 = counter(0);
+c4.display(45, 45);
+// convert signed int to unsigned int binary value
+let signed_to_bin = (int) => {
+    for (let i = bin1.length - 1; i >= 0; i--) {
+        // diff from c2 to int (int will be larger)
+        c3.set(2 ** i);
+        $.add(item_edit(c2.item, c3.item, c4.item, ITEM, ITEM, ITEM, EQ, ADD));
+        let tcond = trigger_function(() => {
+            bin1[bits - 1 - i].add(1);
+            c2.add(2 ** i);
+        });
+        // <= int
+        compare(c4, LESS_OR_EQ, int, tcond);
+        c3.reset();
+        c4.reset();
+    };
+};
+let real_num = 2 ** 32 - 2;
+let it = counter((real_num - 1) - (2 ** 31 - 1));
+wait(0.5);
+signed_to_bin(it);
+
+$.liveEditor({
+    info: true
+});
